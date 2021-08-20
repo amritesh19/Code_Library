0a0ab4ca798cedc43ef4c5465e289ec6805fc73b
Amritesh Anand <73696688+amritesh19@users.noreply.github.com>
2021-08-20 23:41:47 +0530

    Update LCS.cpp

--- String Algorithms/LCS.cpp
+++ String Algorithms/LCS.cpp
@@ -5,6 +5,7 @@
  *  		   if either is empty
  */
  
+
 string lcs(string &a, string &b){
 	int a_size = a.size(), b_size = b.size();
 	
@@ -13,6 +14,10 @@ string lcs(string &a, string &b){
 	
 	int dp[a_size+1][b_size+1];
 	
+	/*
+	 TC : O(n*m)
+	 SC : O(n*m) as we are storing each state
+	*/
 	for(int i=0;i<=a_size;i++){
 		for(int j=0;j<=b_size;j++){
 			if(i==0||j==0) dp[i][j] = 0;
@@ -23,6 +28,7 @@ string lcs(string &a, string &b){
 			}
 		}
 	}
+	
 	
 	//cout<<dp[a_size][b_size]<<"\n";
 	
@@ -43,4 +49,30 @@ string lcs(string &a, string &b){
 	
 	return lcs;
 	
+}
+
+/* Space Optimised LCS of two strings
+   
+   As are only using states of one row above to calculate current row, we can use only
+   two rows, first for prev and second for current and vice versa.
+   
+   TC : O(n*m) 
+   SC : O(2*min(n,m)) = O(min(n,m))
+*/
+
+int lcs(string &a, string &b) {
+  if (a.size() < b.size()) return lcs(b, a);
+  
+  vector<vector<short>> dp(2, vector<short>(b.size() + 1));
+
+  for (int i = 1; i <= (int)a.size(); ++i){
+    for (int j = 1; j <= (int)b.size(); ++j){
+    	
+      	if (a[i - 1] == b[j - 1])
+       		dp[i % 2][j] = dp[(i -1) % 2][j - 1] + 1;
+       		
+      	else dp[i % 2][j] = max(dp[(i - 1) % 2][j], dp[i % 2][j - 1]);
+      	}
+      }
+  return dp[a.size() % 2][b.size()];
 }
